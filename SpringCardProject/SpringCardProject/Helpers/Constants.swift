269be56ff89830752cbe2f374483881a4f53645f
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-02-28 19:23:54 +0200

    Organizing the project to groups and initializing the main files ( Network Manager , Models , Constants)

---
+++ SpringCardProject/SpringCardProject/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SpringCardProject
+//
+//  Created by Marina on 28/02/2022.
+//  File that has all constants of the app
+
+import Foundation
+
+struct Constants {
+    static let API_URL = ""
+    static let API_KEY = "177605c97147407da7bfa8ca7efead44"
+    
+    
+}
---
+++ SpringCardProject/SpringCardProject/Managers/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  SpringCardProject
+//
+//  Created by Marina on 28/02/2022.
+//  Network Manager is a class that handles all things about network and APIs
+
+import Foundation
+
+class NetworkManager{
+    // singleton instance of NetworkManager
+    static let networkManager = NetworkManager()
+    
+    // get the Current currencies Rate
+    func getCurriencesRate(){
+        
+    }
+}
