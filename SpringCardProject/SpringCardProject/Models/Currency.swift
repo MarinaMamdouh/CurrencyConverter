8d668f2855ccdcf73b69db2542265f5a554867b2
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-02-28 19:25:00 +0200

    Add Currency Model

---
+++ SpringCardProject/SpringCardProject/Models/Currency.swift
@@ -0,0 +1,15 @@
+//
+//  Currency.swift
+//  SpringCardProject
+//
+//  Created by Marina on 28/02/2022.
+//  Model represents the data properties of Currency
+
+import Foundation
+
+struct Currency{
+    var symbol:String
+    var rate:Double
+    var date:Date
+    
+}
